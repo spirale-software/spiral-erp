d38ebb4034fb7a953e81ba25d9755814527740b1
LapiEmo <lapigerard@yahoo.fr>
2020-03-01 22:58:37 +0100

    Composant UI, Liste des ventes

--- src/main/webapp/app/spiral-erp/projet/tache/tache-update.component.ts
+++ src/main/webapp/app/spiral-erp/projet/tache/tache-update.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'erp-tache-update',
-  templateUrl: '.tache-update.component.html'
+  templateUrl: './tache-update.component.html'
 })
 export class TacheUpdateComponent {}
--- src/main/webapp/app/spiral-erp/vente/vente.module.ts
+++ src/main/webapp/app/spiral-erp/vente/vente.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { primengLib } from 'app/spiral-erp/shared/primeng-lib';
 import { VenteComponent } from 'app/spiral-erp/vente/vente.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [...primengLib],
+  imports: [...primengLib, RouterModule],
   declarations: [VenteComponent]
 })
 export class VenteModule {}
