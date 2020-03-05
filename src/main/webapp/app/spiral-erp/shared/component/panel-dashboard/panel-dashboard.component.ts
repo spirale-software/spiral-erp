aaa773bf39f3fc70f2a304495f76ef60c7793f4f
LapiEmo <lapigerard@yahoo.fr>
2020-03-05 21:44:08 +0100

    tableau de bord

--- src/main/webapp/app/spiral-erp/shared/component/panel-dashboard/panel-dashboard.component.ts
+++ src/main/webapp/app/spiral-erp/shared/component/panel-dashboard/panel-dashboard.component.ts
@@ -1,18 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'erp-panel-dashboard',
   templateUrl: './panel-dashboard.component.html'
 })
-export class PanelDashboardComponent implements OnInit {
+export class PanelDashboardComponent implements OnInit, AfterViewInit {
   @Input() titre: string;
   @Input() detail: string;
   @Input() value: number;
+  @Input() backgroundColor = '#00A1F1';
+
+  style: any;
 
   constructor() {
     this.titre = '';
     this.detail = '';
     this.value = 0;
+  }
+
+  ngAfterViewInit(): void {
+    this.style = { 'background-color': this.backgroundColor };
   }
 
   ngOnInit(): void {}
