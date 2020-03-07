17756ef8862530b56139877130f15f870b3f8664
LapiEmo <lapigerard@yahoo.fr>
2020-03-07 03:31:26 +0100

    Tableau de bord

--- src/main/webapp/app/spiral-erp/tableau-de-bord/tableau-de-bord.module.ts
+++ src/main/webapp/app/spiral-erp/tableau-de-bord/tableau-de-bord.module.ts
@@ -3,9 +3,10 @@ import { primengLib } from 'app/spiral-erp/shared/const/primeng-lib';
 import { TableauDeBordComponent } from 'app/spiral-erp/tableau-de-bord/tableau-de-bord.component';
 import { SpiralErpSharedModule } from 'app/shared/shared.module';
 import { SharedModule } from 'app/spiral-erp/shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [...primengLib, SpiralErpSharedModule, SharedModule],
+  imports: [...primengLib, SpiralErpSharedModule, SharedModule, RouterModule],
   declarations: [TableauDeBordComponent]
 })
 export class TableauDeBordModule {}
