db81539860bbd6ab283be3b81ec51fcef0faee9a
LapiEmo <lapigerard@yahoo.fr>
2020-03-07 01:49:58 +0100

    Ecran des stocks

--- src/main/webapp/app/spiral-erp/stock/stock.module.ts
+++ src/main/webapp/app/spiral-erp/stock/stock.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { primengLib } from 'app/spiral-erp/shared/const/primeng-lib';
 import { RouterModule } from '@angular/router';
 import { StockComponent } from 'app/spiral-erp/stock/stock.component';
+import { SharedModule } from 'app/spiral-erp/shared/shared.module';
 
 @NgModule({
-  imports: [...primengLib, RouterModule],
+  imports: [...primengLib, RouterModule, SharedModule],
   declarations: [StockComponent]
 })
 export class StockModule {}
