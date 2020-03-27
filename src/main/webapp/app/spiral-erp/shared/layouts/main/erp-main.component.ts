e9cc7ca8045d10cf60f91891c0073dd5c00eb007
LapiEmo <lapigerard@yahoo.fr>
2020-03-27 23:03:25 +0100

    Administration

--- src/main/webapp/app/spiral-erp/shared/layouts/main/erp-main.component.ts
+++ src/main/webapp/app/spiral-erp/shared/layouts/main/erp-main.component.ts
@@ -14,56 +14,54 @@ export class ErpMainComponent implements OnInit {
     console.log('innerWidth: ', window.innerWidth);
   }
 
-  isLargeScreen(): boolean {
-    return window.innerWidth > 1024;
-  }
+  closeSidebar = () => {
+    this.display = false;
+  };
 
   ngOnInit(): void {
     this.items = [
       {
         label: '',
-        items: [{ label: 'Tableau de bord', icon: 'pi pi-chart-bar', routerLink: 'tableau-de-bord' }]
+        items: [{ label: 'Tableau de bord', icon: 'pi pi-chart-bar', routerLink: 'tableau-de-bord', command: this.closeSidebar }]
       },
       {
         label: "Gestion d'entreprise",
         items: [
-          { label: 'Achat', icon: 'pi pi-fw pi-shopping-cart', routerLink: 'achats' },
-          { label: 'Vente', icon: 'pi pi-dollar', routerLink: 'ventes' },
-          { label: 'Dépense', icon: 'pi pi-minus', routerLink: 'depenses' }
+          { label: 'Achat', icon: 'pi pi-fw pi-shopping-cart', routerLink: 'achats', command: this.closeSidebar },
+          { label: 'Vente', icon: 'pi pi-dollar', routerLink: 'ventes', command: this.closeSidebar },
+          { label: 'Dépense', icon: 'pi pi-minus', routerLink: 'depenses', command: this.closeSidebar }
         ]
       },
       {
         label: 'Gestion de projet',
-        items: [{ label: 'Projet', icon: 'pi pi-fw pi-share-alt', routerLink: 'projets' }]
+        items: [{ label: 'Projet', icon: 'pi pi-fw pi-share-alt', routerLink: 'projets', command: this.closeSidebar }]
       },
       {
         label: 'Employés',
         items: [
-          { label: 'Gestion des employés', icon: 'pi pi-users', routerLink: 'employes' },
-          { label: 'Gestion des droits et accès', icon: 'pi pi-user-plus', routerLink: 'gestion-des-droits' }
+          { label: 'Gestion des employés', icon: 'pi pi-users', routerLink: 'employes', command: this.closeSidebar },
+          { label: 'Gestion des droits et accès', icon: 'pi pi-user-plus', routerLink: 'gestion-des-droits', command: this.closeSidebar }
         ]
       },
       {
         label: 'Comptabilité',
         items: [
-          { label: 'Stocks', icon: 'pi pi-home', routerLink: 'stocks' },
-          { label: 'Bilan', icon: 'pi pi-table', routerLink: 'bilans' }
+          { label: 'Stocks', icon: 'pi pi-home', routerLink: 'stocks', command: this.closeSidebar },
+          { label: 'Bilan', icon: 'pi pi-table', routerLink: 'bilans', command: this.closeSidebar }
         ]
-      }
-      /* {
+      },
+      {
         label: 'Administration',
         items: [
-          { label: 'Gestion des utilisateurs', icon: 'pi pi-home', routerLink: 'stocks' },
-          { label: 'Suivi des utilisateurs', icon: 'pi pi-table', routerLink: 'bilans' },
-          { label: 'Métriques', icon: 'pi pi-table', routerLink: 'bilans' },
-          { label: 'Diagnostics', icon: 'pi pi-table', routerLink: 'bilans' },
-          { label: 'Configuration', icon: 'pi pi-table', routerLink: 'bilans' },
-          { label: 'Logs', icon: 'pi pi-table', routerLink: 'bilans' },
-          { label: 'Audits', icon: 'pi pi-table', routerLink: 'bilans' },
-          { label: 'API', icon: 'pi pi-table', routerLink: 'bilans' },
-          { label: 'Base de données', icon: 'pi pi-table', routerLink: 'bilans' }
+          { label: 'Gestion des utilisateurs', icon: 'pi pi-user', routerLink: 'admin/user-management', command: this.closeSidebar },
+          { label: 'Métriques', icon: 'pi pi-th-large', routerLink: 'admin/metrics', command: this.closeSidebar },
+          { label: 'Diagnostics', icon: 'pi pi-star-o', routerLink: 'admin/health', command: this.closeSidebar },
+          { label: 'Configuration', icon: 'pi pi-list', routerLink: 'admin/configuration', command: this.closeSidebar },
+          { label: 'Audits', icon: 'pi pi-bell', routerLink: 'admin/audits', command: this.closeSidebar },
+          { label: 'Logs', icon: 'pi pi-align-justify', routerLink: 'admin/logs', command: this.closeSidebar },
+          { label: 'API', icon: 'pi pi-table', routerLink: 'admin/docs', command: this.closeSidebar }
         ]
-      }*/
+      }
     ];
   }
 }
