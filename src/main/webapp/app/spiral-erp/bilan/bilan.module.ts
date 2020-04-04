75cdb39c1b7b909d2ca5230e3f0c327a0d9d1d89
LapiEmo <lapigerard@yahoo.fr>
2020-04-04 14:01:13 +0200

    refactoring bilan

--- src/main/webapp/app/spiral-erp/bilan/bilan.module.ts
+++ src/main/webapp/app/spiral-erp/bilan/bilan.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { primengLib } from 'app/spiral-erp/shared/const/primeng-lib';
 import { RouterModule } from '@angular/router';
 import { BilanComponent } from 'app/spiral-erp/bilan/bilan.component';
+import { SpiralErpSharedModule } from 'app/shared/shared.module';
 
 @NgModule({
-  imports: [...primengLib, RouterModule],
+  imports: [...primengLib, RouterModule, SpiralErpSharedModule],
   declarations: [BilanComponent]
 })
 export class BilanModule {}
