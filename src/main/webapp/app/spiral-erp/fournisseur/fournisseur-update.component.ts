4e1bb83e47bcfe1a3ccb16c385362c1ef5312d71
LapiEmo <lapigerard@yahoo.fr>
2020-04-08 20:37:25 +0200

    ecran des fournisseurs

--- src/main/webapp/app/spiral-erp/fournisseur/fournisseur-update.component.ts
+++ src/main/webapp/app/spiral-erp/fournisseur/fournisseur-update.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'erp-fournisseur-update',
   templateUrl: './fournisseur-update.component.html'
 })
 export class FournisseurUpdateComponent implements OnInit {
-  constructor() {}
+  titre: string | undefined;
 
-  ngOnInit(): void {}
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.params['id'];
+    if (id) {
+      this.titre = 'Détail/Modification fournisseur';
+    } else {
+      this.titre = 'Créer nouveau fournisseur';
+    }
+  }
 
   back(): void {
     window.history.back();
