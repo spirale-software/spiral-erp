9702ebd5a7f518d3082d9926227ca4f71c36be79
LapiEmo <lapigerard@yahoo.fr>
2020-04-17 15:39:17 +0200

    utilisateur

--- src/main/webapp/app/spiral-erp/utilisateur/utilisateur.component.ts
+++ src/main/webapp/app/spiral-erp/utilisateur/utilisateur.component.ts
@@ -1,33 +1,88 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ITEMS_PER_PAGE } from 'app/shared/constants/pagination.constants';
+import { HttpHeaders, HttpResponse } from '@angular/common/http';
+import { IAudit } from 'app/shared/model/audit.model';
+import { UtilisateurErpService } from 'app/spiral-erp/utilisateur/utilisateur-erp.service';
+import { Utilisateur } from 'app/shared/model/utilisateur.model';
+import { JhiParseLinks } from 'ng-jhipster';
 
 @Component({
   selector: 'erp-utilisateur',
   templateUrl: './utilisateur.component.html'
 })
 export class UtilisateurComponent {
-  utilisateurs: any[];
+  utilisateurs: Utilisateur[] | null;
+  eventSubscriber?: Subscription;
+  itemsPerPage: number;
+  links: any;
+  page: number;
+  predicate: string;
+  ascending: boolean;
 
-  constructor() {
-    this.utilisateurs = [
-      {
-        nom: 'Casimir',
-        prenom: 'Junior',
-        login: 'gyl',
-        email: 'gyl@spiral.be',
-        telephone: '04555555',
-        actif: true,
-        entreprise: 'Spiral-ERP'
-      },
-      {
-        nom: 'Ashanti',
-        prenom: 'reine',
-        login: 'arn',
-        email: 'arn@spiral.be',
-        telephone: '04555555',
-        actif: true,
-        entreprise: 'Spiral-MEDIA'
-      },
-      { nom: 'Toure', prenom: 'Daomey', login: 'tdy', email: 'tdy@spiral.be', telephone: '04555555', actif: true, entreprise: 'Spiral-ERP' }
-    ];
+  constructor(private utilisateurErpService: UtilisateurErpService, protected parseLinks: JhiParseLinks) {
+    this.utilisateurs = null;
+    this.itemsPerPage = ITEMS_PER_PAGE;
+    this.page = 0;
+    this.links = {
+      last: 0
+    };
+    this.predicate = 'id';
+    this.ascending = true;
+  }
+
+  ngOnInit(): void {
+    this.loadAll();
+  }
+
+  loadAll(critereTransversal?: any): void {
+    this.utilisateurs = null;
+    const req = {
+      page: this.page,
+      size: this.itemsPerPage,
+      sort: this.sort()
+    };
+    if (critereTransversal) {
+      req['critereTransversal'] = critereTransversal;
+    }
+    this.utilisateurErpService.query(req).subscribe((res: HttpResponse<IAudit[]>) => this.paginateUtilisateurs(res.body, res.headers));
+  }
+
+  findAll(critereTransversal: any): void {
+    this.page = 0;
+    this.utilisateurs = [];
+    this.loadAll(critereTransversal);
+  }
+
+  sort(): string[] {
+    const result = [this.predicate + ',' + (this.ascending ? 'asc' : 'desc')];
+    if (this.predicate !== 'id') {
+      result.push('id');
+    }
+    return result;
+  }
+
+  reset(): void {
+    this.page = 0;
+    this.utilisateurs = [];
+    this.loadAll();
+  }
+
+  loadPage(page: number): void {
+    this.page = page;
+    this.loadAll();
+  }
+
+  protected paginateUtilisateurs(data: IAudit[] | null, headers: HttpHeaders): void {
+    if (!this.utilisateurs) {
+      this.utilisateurs = [];
+    }
+    const headersLink = headers.get('link');
+    this.links = this.parseLinks.parse(headersLink ? headersLink : '');
+    if (data) {
+      for (let i = 0; i < data.length; i++) {
+        if (this.utilisateurs) this.utilisateurs.push(data[i]);
+      }
+    }
   }
 }
--- src/main/webapp/app/spiral-erp/utilisateur/utilisateur.module.ts
+++ src/main/webapp/app/spiral-erp/utilisateur/utilisateur.module.ts
@@ -3,9 +3,11 @@ import { primengLib } from 'app/spiral-erp/shared/const/primeng-lib';
 import { RouterModule } from '@angular/router';
 import { UtilisateurComponent } from 'app/spiral-erp/utilisateur/utilisateur.component';
 import { UtilisateurUpdateComponent } from 'app/spiral-erp/utilisateur/utilisateur-update.component';
+import { SpiralErpSharedModule } from 'app/shared/shared.module';
+import { SharedModule } from 'app/spiral-erp/shared/shared.module';
 
 @NgModule({
-  imports: [...primengLib, RouterModule],
+  imports: [...primengLib, RouterModule, SpiralErpSharedModule, SharedModule],
   declarations: [UtilisateurComponent, UtilisateurUpdateComponent]
 })
 export class UtilisateurModule {}
