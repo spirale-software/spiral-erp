402b02126fedffc6f35e7121aa0de53deb12973a
LapiEmo <lapigerard@yahoo.fr>
2020-04-18 14:53:48 +0200

    Entreprise

--- src/main/webapp/app/spiral-erp/entreprise/entreprise-update.component.ts
+++ src/main/webapp/app/spiral-erp/entreprise/entreprise-update.component.ts
@@ -1,72 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ArticleErpService } from 'app/spiral-erp/article/article-erp.service';
-import { IArticle } from 'app/shared/model/article.model';
+import { EntrepriseErpService } from 'app/spiral-erp/entreprise/entreprise-erp.service';
+import { IEntreprise } from 'app/shared/model/entreprise.model';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { SelectItem } from 'primeng/api';
-import { FournisseurErpService } from 'app/spiral-erp/fournisseur/fournisseur-erp.service';
 import { JhiEventManager } from 'ng-jhipster';
 
 @Component({
-  selector: 'erp-article-update',
+  selector: 'erp-entreprise-update',
   templateUrl: './entreprise-update.component.html'
 })
 export class EntrepriseUpdateComponent implements OnInit {
   titre: string | undefined;
-  article: IArticle;
-  articleForm: FormGroup;
-  fournisseurOptions: SelectItem[];
+  entreprise: IEntreprise;
+  entrepriseForm: FormGroup;
 
   constructor(
     private route: ActivatedRoute,
-    private articleErpService: ArticleErpService,
-    private fournisseurErpService: FournisseurErpService,
+    private entrepriseErpService: EntrepriseErpService,
     private fb: FormBuilder,
     private router: Router,
     private eventManager: JhiEventManager
   ) {
-    this.articleForm = {} as FormGroup;
-    this.article = {} as IArticle;
-    this.fournisseurOptions = [{ label: '', value: null }];
+    this.entrepriseForm = {} as FormGroup;
+    this.entreprise = {} as IEntreprise;
     this.createForm();
-    this.setFournisseurOptions();
   }
 
   createForm(): void {
-    this.articleForm = this.fb.group({
-      nom: [this.article.nom],
-      code: [this.article.code],
-      fournisseur: []
+    this.entrepriseForm = this.fb.group({
+      nom: [],
+      actif: []
     });
-  }
-
-  setFournisseurOptions(): void {
-    this.fournisseurErpService
-      .query()
-      .toPromise()
-      .then(httpReponse => {
-        if (httpReponse.body) {
-          httpReponse.body.forEach(item => this.fournisseurOptions.push({ label: item.nom, value: item }));
-        }
-      })
-      .catch(httpError => console.log(httpError));
   }
 
   ngOnInit(): void {
     const id = this.route.snapshot.params['id'];
     if (id) {
-      this.titre = 'Détail/Modification article';
-      this.articleErpService
+      this.titre = 'Détail/Modification entreprise';
+      this.entrepriseErpService
         .find(id)
         .toPromise()
         .then(httpResponse => {
           if (httpResponse.body) {
-            this.article = httpResponse.body;
-            this.articleForm.patchValue(this.article);
+            this.entreprise = httpResponse.body;
+            this.entrepriseForm.patchValue(this.entreprise);
           }
         });
     } else {
-      this.titre = 'Créer nouvel article';
+      this.titre = 'Créer nouvel entreprise';
     }
   }
 
@@ -75,27 +56,27 @@ export class EntrepriseUpdateComponent implements OnInit {
   }
 
   save(): void {
-    this.article = Object.assign(this.article, this.articleForm.value);
-    console.log(this.article);
-    if (!this.article.id) {
-      this.articleErpService
-        .create(this.article)
+    this.entreprise = Object.assign(this.entreprise, this.entrepriseForm.value);
+    console.log(this.entreprise);
+    if (!this.entreprise.id) {
+      this.entrepriseErpService
+        .create(this.entreprise)
         .toPromise()
         .then(() => {
-          this.router.navigate(['articles']);
+          this.router.navigate(['entreprises']);
           const summary = 'Sauvegarde réussie';
-          const detail = "L'article a bien été crée";
+          const detail = "L'entreprise a bien été crée";
           this.showMessage('success', summary, detail);
         })
         .catch(httpError => console.log(httpError));
     } else {
-      this.articleErpService
-        .update(this.article)
+      this.entrepriseErpService
+        .update(this.entreprise)
         .toPromise()
         .then(() => {
-          this.router.navigate(['articles']);
+          this.router.navigate(['entreprises']);
           const summary = 'Mise à jour réussie.';
-          const detail = "L'article a bien été mis à jour.";
+          const detail = "L'entreprise a bien été mis à jour.";
           this.showMessage('success', summary, detail);
         })
         .catch(httpError => console.log(httpError));
