1476e7f416e56effd47cdab57d293f65f60c0984
LapiEmo <lapigerard@yahoo.fr>
2020-04-19 03:04:01 +0200

    vente, depense

--- src/main/java/io/spiral/erp/app/service/dto/VenteDTO.java
+++ src/main/java/io/spiral/erp/app/service/dto/VenteDTO.java
@@ -1,4 +1,87 @@
 package io.spiral.erp.app.service.dto;
 
+import java.time.ZonedDateTime;
+
 public class VenteDTO {
+    private Long id;
+    private Long idVendeur;
+    private String nomVendeur;
+    private ZonedDateTime dateVente;
+    private String nomProduit;
+    private Long idProduit;
+    private Double prixUnitaireVente;
+    private Integer quantiteVendu;
+    private Double tauxTVA;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdVendeur() {
+        return idVendeur;
+    }
+
+    public void setIdVendeur(Long idVendeur) {
+        this.idVendeur = idVendeur;
+    }
+
+    public String getNomVendeur() {
+        return nomVendeur;
+    }
+
+    public void setNomVendeur(String nomVendeur) {
+        this.nomVendeur = nomVendeur;
+    }
+
+    public ZonedDateTime getDateVente() {
+        return dateVente;
+    }
+
+    public void setDateVente(ZonedDateTime dateVente) {
+        this.dateVente = dateVente;
+    }
+
+    public String getNomProduit() {
+        return nomProduit;
+    }
+
+    public void setNomProduit(String nomProduit) {
+        this.nomProduit = nomProduit;
+    }
+
+    public Long getIdProduit() {
+        return idProduit;
+    }
+
+    public void setIdProduit(Long idProduit) {
+        this.idProduit = idProduit;
+    }
+
+    public Double getPrixUnitaireVente() {
+        return prixUnitaireVente;
+    }
+
+    public void setPrixUnitaireVente(Double prixUnitaireVente) {
+        this.prixUnitaireVente = prixUnitaireVente;
+    }
+
+    public Integer getQuantiteVendu() {
+        return quantiteVendu;
+    }
+
+    public void setQuantiteVendu(Integer quantiteVendu) {
+        this.quantiteVendu = quantiteVendu;
+    }
+
+    public Double getTauxTVA() {
+        return tauxTVA;
+    }
+
+    public void setTauxTVA(Double tauxTVA) {
+        this.tauxTVA = tauxTVA;
+    }
 }
--- src/main/java/io/spiral/erp/app/service/dto/depenseDTO.java
+++ src/main/java/io/spiral/erp/app/service/dto/depenseDTO.java
@@ -1,4 +1,51 @@
 package io.spiral.erp.app.service.dto;
 
+import java.time.ZonedDateTime;
+
 public class depenseDTO {
+    private Long id;
+    private String description;
+    private String typeDepense;
+    private ZonedDateTime dateDepense;
+    private Double montant;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTypeDepense() {
+        return typeDepense;
+    }
+
+    public void setTypeDepense(String typeDepense) {
+        this.typeDepense = typeDepense;
+    }
+
+    public ZonedDateTime getDateDepense() {
+        return dateDepense;
+    }
+
+    public void setDateDepense(ZonedDateTime dateDepense) {
+        this.dateDepense = dateDepense;
+    }
+
+    public Double getMontant() {
+        return montant;
+    }
+
+    public void setMontant(Double montant) {
+        this.montant = montant;
+    }
 }
